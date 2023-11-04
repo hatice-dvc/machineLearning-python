b6b5a715716a7c836fe7c4d7a261f13305680631
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-05 00:49:40 +0300

    Initial commit

---
+++ data_structures.py
@@ -0,0 +1,35 @@
+###############################################
+# VERİ YAPILARI (DATA STRUCTURES)
+###############################################
+# - Veri Yapılarına Giriş ve Hızlı Özet
+# - Sayılar (Numbers): int, float, complex
+# - Karakter Dizileri (Strings): str
+# - Boolean (TRUE-FALSE): bool
+# - Liste (List)
+# - Sözlük (Dictionary)
+# - Demet (Tuple)
+# - Set
+
+
+###############################################
+# Veri Yapılarına Giriş ve Hızlı Özet
+##############################################
+
+x = 46
+type(x)
+
+y = 10.3
+type(y)
+
+z = 2j + 1
+type(z)
+
+d = "hello ai era"
+type(d)
+
+type(5==4)
+
+
+#liste
+
+
