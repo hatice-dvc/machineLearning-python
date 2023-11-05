a7e51d448501bd240941220560585013c3bed967
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-05 17:26:02 +0300

    data structures

---
+++ python_for_datascience/data_structure.py
@@ -0,0 +1,36 @@
+###############################################
+# VERİ YAPILARI (DATA STRUCTURES)
+###############################################
+
+# - Sayılar (Numbers): int, float, complex
+# - Karakter Dizileri (Strings): str
+# - Boolean (TRUE-FALSE): bool
+# - Liste (List)
+# - Sözlük (Dictionary)
+# - Demet (Tuple)
+# - Set
+
+###############################################
+# Sayılar (Numbers): int, float, complex
+###############################################
+
+a = 10
+b = 10.5
+
+a * 3
+a / 7
+a * b / 10
+a ** 2
+
+#######################
+# Tipleri değiştirmek
+#######################
+
+int(b)
+float(a)
+
+int(a * b / 10)
+
+c = a * b / 10
+
+int(c)
