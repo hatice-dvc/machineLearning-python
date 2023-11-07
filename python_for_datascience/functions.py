0d1f8d1cc50ed6d513f3b94e936cf34d820729c0
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-07 16:26:29 +0300

    functions

---
+++ python_for_datascience/functions.py
@@ -0,0 +1,22 @@
+###############################################
+# FONKSİYONLAR (FUNCTIONS)
+###############################################
+
+#######################
+# Fonksiyon Tanımlama
+#######################
+def calculate(x):
+    print(x * 2)
+
+
+calculate(5)
+
+# İki argümanlı/parametreli bir fonksiyon tanımlayalım.
+
+
+def summer(arg1, arg2):
+    print(arg1 + arg2)
+
+
+summer(7, 8)
+summer(arg2=8, arg1=7)
