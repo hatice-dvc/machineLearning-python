cc5f7bb0bd17cc7728b9038e962d3548cc744c2c
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-09 21:28:56 +0300

    conditions

---
+++ python_for_datascience/conditions.py
@@ -0,0 +1,54 @@
+###############################################
+# KOŞULLAR (CONDITIONS)
+###############################################
+
+# if
+if 1 == 1:
+    print("something")
+
+if 1 == 2:
+    print("something")
+
+number = 11
+if number == 10:
+    print("number is 10")
+
+number = 10
+
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+
+
+number_check(12)
+
+#######################
+# else
+#######################
+
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+    else:
+        print("number is not 10")
+
+
+number_check(12)
+
+#######################
+# elif
+#######################
+
+
+def number_check(number):
+    if number > 10:
+        print("greater than 10")
+    elif number < 10:
+        print("less than 10")
+    else:
+        print("equal to 10")
+
+
+number_check(6)
