cf96e1011c5559eef41ba8a4f61d420f97e4ac02
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-12 20:34:02 +0300

    loops

---
+++ python_for_datascience/loops.py
@@ -0,0 +1,72 @@
+###############################################
+# DÖNGÜLER (LOOPS)
+###############################################
+# for loop
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+students[0]
+students[1]
+students[2]
+
+for student in students:
+    print(student)
+
+for student in students:
+    print(student.upper())
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    print(salary)
+
+for salary in salaries:
+    print(int(salary*20/100 + salary))
+
+
+def new_salary(salary, rate):
+    return int(salary*rate/100 + salary)
+
+
+new_salary(1500, 10)
+new_salary(2000, 20)
+
+for salary in salaries:
+    print(new_salary(salary, 20))
+
+
+salaries2 = [10700, 25000, 30400, 40300, 50200]
+
+for salary in salaries2:
+    print(new_salary(salary, 15))
+
+for salary in salaries:
+    if salary >= 3000:
+        print(new_salary(salary, 10))
+    else:
+        print(new_salary(salary, 20))
+
+#######################
+# Uygulama
+#######################
+
+# Amaç: Aşağıdaki şekilde string değiştiren fonksiyon yazmak istiyoruz.
+
+# before: "hi my name is john and i am learning python"
+# after: "Hi mY NaMe iS JoHn aNd i aM LeArNiNg pYtHoN"
+
+
+def alternating(string):
+    new_string = ""
+    # girilen string'in index'lerinde gez.
+    for string_index in range(len(string)):
+        # index çift ise büyük harfe çevir.
+        if string_index % 2 == 0:
+            new_string += string[string_index].upper()
+        # index tek ise küçük harfe çevir.
+        else:
+            new_string += string[string_index].lower()
+    print(new_string)
+
+
+alternating("hi my name is john and i am learning python")
