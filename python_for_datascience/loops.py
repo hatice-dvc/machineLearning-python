de19a688643abd89441d34d5d3f57edcdaaf5a9b
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-23 21:46:28 +0300

    loops

--- python_for_datascience/loops.py
+++ python_for_datascience/loops.py
@@ -70,3 +70,28 @@ def alternating(string):
 
 
 alternating("hi my name is john and i am learning python")
+
+#######################
+# break & continue & while
+#######################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    if salary == 3000:
+        break
+    print(salary)
+
+
+for salary in salaries:
+    if salary == 3000:
+        continue
+    print(salary)
+
+
+# while
+
+number = 1
+while number < 5:
+    print(number)
+    number += 1
