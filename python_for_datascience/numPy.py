3f3e94e88637dd2b68eb4e8315a8896ce4778c22
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2024-10-27 16:27:41 +0300

    numPy

---
+++ python_for_datascience/numPy.py
@@ -0,0 +1,55 @@
+#############################################
+# NUMPY
+#############################################
+
+import numpy as np
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+a * b
+
+#############################################
+# Creating Numpy Arrays
+#############################################
+
+import numpy as np
+
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5]))
+np.zeros(10, dtype=int)
+np.random.randint(0, 10, size=10)
+np.random.normal(10, 4, (3, 4))
+
+#############################################
+# Attributes of Numpy Arrays
+#############################################
+import numpy as np
+
+# ndim: boyut sayısı
+# shape: boyut bilgisi
+# size: toplam eleman sayısı
+# dtype: array veri tipi
+
+a = np.random.randint(10, size=5)
+a.ndim
+a.shape
+a.size
+a.dtype
+
+#############################################
+# Reshaping
+#############################################
+import numpy as np
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size=9)
+ar.reshape(3, 3)
