e122ae7c022d1d4c583168cf7fda37915354542f
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2024-11-03 22:31:48 +0300

    pandas

--- python_for_datascience/pandas.py
+++ python_for_datascience/pandas.py
@@ -17,3 +17,72 @@ s.values
 type(s.values)
 s.head(3)
 s.tail(3)
+
+df = pd.read_csv("python_for_datascience/datasets/advertising.csv")
+df.head()
+
+#############################################
+# Veriye Hızlı Bakış (Quick Look at Data)
+#############################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+df["sex"].head()
+df["sex"].value_counts()
+
+#############################################
+# Pandas'ta Seçim İşlemleri (Selection in Pandas)
+#############################################
+import pandas as pd
+import seaborn as sns
+df = sns.load_dataset("titanic")
+df.head()
+
+df.index
+df[0:13]
+df.drop(0, axis=0).head()
+
+delete_indexes = [1, 3, 5, 7]
+df.drop(delete_indexes, axis=0).head(10)
+
+# df = df.drop(delete_indexes, axis=0)
+# df.drop(delete_indexes, axis=0, inplace=True)
+
+#######################
+# Değişkeni Indexe Çevirmek
+#######################
+
+df["age"].head()
+df.age.head()
+
+df.index = df["age"]
+
+df.drop("age", axis=1).head()
+
+df.drop("age", axis=1, inplace=True)
+df.head()
+
+#######################
+# Indexi Değişkene Çevirmek
+#######################
+
+df.index
+
+df["age"] = df.index
+
+df.head()
+df.drop("age", axis=1, inplace=True)
+
+df.reset_index().head()
+df = df.reset_index()
+df.head()
